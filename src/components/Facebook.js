753befe0dba4c378834395e27087cacad585e211
xovest <79008917+xovest@users.noreply.github.com>
2021-11-10 17:48:22 +0200

    go for the login

---
+++ src/components/Facebook.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import FacebookLogin from 'react-facebook-login';
+
+export default class Facebook extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
